0b0baf8726ae37c7a48364b396b266c87fda2f4d
Alexandr Florenko <AlexandrFlor@yandex.ru>
2022-07-20 20:44:04 +0400

    add Testcontainers

--- src/test/java/as/florenko/conditional/ConditionalApplicationTests.java
+++ src/test/java/as/florenko/conditional/ConditionalApplicationTests.java
@@ -1,13 +1,40 @@
 package as.florenko.conditional;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ConditionalApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private TestRestTemplate restTemplate;
+    @Container
+    public static final GenericContainer<?> devProf = new GenericContainer<>("devapp").withExposedPorts(8080);
+    @Container
+    public static final GenericContainer<?> prodProf = new GenericContainer<>("prodapp").withExposedPorts(8081);
+
+    @BeforeAll
+    public static void setUp() {
+        devProf.start();
+        prodProf.start();
     }
 
+    @Test
+    void devProfileTest() {
+        String responseBodyDev = restTemplate.getForObject("http://localhost:" + devProf.getMappedPort(8080) + "/profile", String.class);
+        Assertions.assertEquals("Current profile is dev", responseBodyDev);
+    }
+
+    @Test
+    void productionProfileTest() {
+        String responseBodyProd = restTemplate.getForObject("http://localhost:" + prodProf.getMappedPort(8081) + "/profile", String.class);
+        Assertions.assertEquals("Current profile is production", responseBodyProd);
+    }
 }
